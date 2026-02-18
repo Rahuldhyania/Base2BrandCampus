3866cd581e406e9eec76584dd2271e553479352b
techbase2brand <techbase2brand@gmail.com>
2026-02-18 17:38:03 +0530

    seo team chnages

--- src/app/category/top-courses/page.jsx
+++ src/app/category/top-courses/page.jsx
@@ -2,7 +2,7 @@ import PageBanner from "@/app/Components/Page-Components/PageBanner";
 import React from "react";
 
 export const metadata = {
-  title: "Title: Top Professional Courses | B2B Campus Training Programs",
+  title: "Top Professional Courses | B2B Campus Training Programs",
   description:
     "Explore top industry-focused training courses at B2B Campus including Truck Dispatch, Freight Broker, Cyber Security, Devops, and more with practical learning."
 };
